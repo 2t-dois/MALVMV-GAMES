fe455bece352dd3a82ccc97b512298f9b3498132
2T <matheus.ffranca09@gmail.com>
2026-05-22 18:11:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,21 @@ const informacoes = [
   },
 
   {
-    titulo: "Primeiro registro",
+    titulo: "Semana 8",
     texto:
-      "Neste bimestre fizemos o primeiro registro do nosso projeto e seguimos evoluindo cada vez mais.",
+      "Foi desenvolvida uma atividade prática voltada para testes de login em um sistema, utilizando casos de teste para validar o funcionamento correto da aplicação. Foram criados cenários de sucesso e erro, analisando login com dados válidos e senha incorreta. O trabalho também apresentou precondições, passos de execução e resultados esperados para cada situação. Além disso, foi destacada a importância de testar falhas para encontrar bugs e melhorar a segurança do sistema.",
+  },
+
+  {
+    titulo: "Semana 9",
+    texto:
+      "Foi realizada uma análise da qualidade dos testes dos módulos do sistema, utilizando KPI-DF, taxa de sucesso e cobertura de requisitos. O estudo identificou que o módulo Login apresenta alto risco devido à grande quantidade de defeitos proporcionalmente ao código. Já o módulo Pagamento mostrou maior risco de falha por possuir baixa taxa de sucesso nos testes. Além disso, o módulo Relatórios apresentou falhas na cobertura de requisitos, necessitando melhorias nos testes realizados.",
+  },
+
+  {
+    titulo: "Semana 10",
+    texto:
+      "O trabalho apresentou duas formas de lidar com mudanças em projetos: a abordagem de contrato rígido e a colaboração Ágil. Foram analisadas as respostas dadas ao cliente e os impactos de cada postura na equipe e no relacionamento com o cliente. Também foi destacado como a comunicação colaborativa reduz conflitos e melhora o trabalho em equipe. Por fim, concluiu-se que o principal objetivo de um projeto é entregar valor ao cliente.",
   },
 
   {
